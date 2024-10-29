bee25c24c8936b53af938dd16ca6ba13d894d247
dengyijian <dengyijian163@163.com>
2024-10-29 21:47:49 +0800

    feat(icon): app icon

--- gui_app/main_gui.go
+++ gui_app/main_gui.go
@@ -1,8 +1,17 @@
 package gui_app
 
-import "fyne.io/fyne/v2/app"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	_icon "github.com/cloudflare-fans/cf-ddns-gui/gui_app/icon"
+)
 
 func (_this *App) initMainApp() {
 	a := app.New()
+	icon := fyne.StaticResource{
+		StaticName:    "x",
+		StaticContent: _icon.Data,
+	}
+	a.SetIcon(&icon)
 	_this.mainApp = &a
 }
